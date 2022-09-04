4e4c493b5a13148d21c8c2d41c9bbc8ca902b68c
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-09-04 19:26:29 +0530

    [Vinaya]Add: Attend

---
+++ EmployeeProblem9/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeProblem9
+{
+    public class Employee
+    {
+        public void Wage()
+        {
+            int FULL_TIME = 1;
+            Random random = new Random();
+            int aCheck = random.Next(0, 2);
+            if (aCheck == FULL_TIME)
+            {
+                Console.WriteLine("The Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("The Employee is absent");
+            }
+        }
+    }
+}
--- EmployeeProblem9/Program.cs
+++ EmployeeProblem9/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee wage computation");
+            Employee employee = new Employee();
+            employee.Wage();
         }
     }
 }
