11be43068479e22b2af71fd61beb2971ca5d27c0
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-09-04 19:21:36 +0530

    Welcome

---
+++ EmployeeProblem9/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeProblem9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee wage computation");
+        }
+    }
+}
