18f073256d4132896d7c275f5d9b619fa2d15fb9
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-09-04 20:49:28 +0530

    [Vinaya]Add: Wage of Companies

--- EmployeeProblem9/Employee.cs
+++ EmployeeProblem9/Employee.cs
@@ -9,45 +9,25 @@ namespace EmployeeProblem9
     // Already using class 
     public class Employee  
     {
-        public void Wage()
+        public int FULL_TIME = 1;
+        int empHr = 0;
+        int empWage = 0;
+        public void Wage(string company, int wagePerHr)
         {
-            const int FULL_TIME = 1;
-            const int PART_TIME = 2;
-            int empHrs = 0;
-            int wagePerHr = 20;
-            int empWage = 0;
-            int totalWorkingdays = 20;
-            int totalWorkingHrs = 100;
-            int totalEmpHr = 0;
-            int totalDay = 0;
-            do
+            Random random = new Random();
+            int aCheck = random.Next(0, 2);
+            if (aCheck == FULL_TIME)
             {
-                //new operator creates an object from the class
-                Random random = new Random();
-                int aCheck = random.Next(0, 3);
-                switch (aCheck)
-                {
-                    case FULL_TIME:
-                        empHrs = 8;
-                        Console.WriteLine("The Employee is present Full Time");
-                        break;
-                    case PART_TIME:
-                        empHrs = 4;
-                        Console.WriteLine("The Employee is present Part Time");
-                        break;
-                    default:
-                        empHrs = 0;
-                        Console.WriteLine("The Employee is absent");
-                        break;
-                }
-                totalEmpHr = totalEmpHr + empHrs;
-                empWage = empHrs * wagePerHr;
-                Console.WriteLine("Employee wage is "+empWage);
-                totalDay++;
-            } while (totalEmpHr <= totalWorkingHrs && totalDay < totalWorkingdays);
-            Console.WriteLine("The total working days per month is " +totalWorkingdays+ " and total working hours is "+totalEmpHr);
-            empWage = totalEmpHr * wagePerHr;
-            Console.WriteLine("Employee wage is "+empWage);
+                empHr = 8;
+                Console.WriteLine("The Employee is present");
+            }
+            else
+            {
+                empHr = 0;
+                Console.WriteLine("The Employee is absent");
+            }
+            empWage = empHr * wagePerHr;
+            Console.WriteLine("The Employee wage of " +company+ " is "+empWage);
         }
     }
 }
--- EmployeeProblem9/Program.cs
+++ EmployeeProblem9/Program.cs
@@ -6,7 +6,10 @@
         {
             Console.WriteLine("Welcome to Employee wage computation");
             Employee employee = new Employee();
-            employee.Wage();
+            employee.Wage("Zoro",20);
+            employee.Wage("Airo", 15);
+            employee.Wage("Gojo", 30);
+
         }
     }
 }
