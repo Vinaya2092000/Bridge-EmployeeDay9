731d55745c21e3cecaa2770372d43990d46579f4
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-09-04 19:54:19 +0530

    [Vinaya]Add: Calculate wage

--- EmployeeProblem9/Employee.cs
+++ EmployeeProblem9/Employee.cs
@@ -11,16 +11,24 @@ namespace EmployeeProblem9
         public void Wage()
         {
             int FULL_TIME = 1;
+            int empHrs = 0;
+            int wagePerHr = 20;
+            int empWage = 0;
+            //new operator creates an object from the class
             Random random = new Random();
             int aCheck = random.Next(0, 2);
             if (aCheck == FULL_TIME)
             {
+                empHrs = 8;
                 Console.WriteLine("The Employee is present");
             }
             else
             {
+                empHrs = 0;
                 Console.WriteLine("The Employee is absent");
             }
+            empWage = empHrs * wagePerHr;
+            Console.WriteLine("The Employee wage is "+empWage);
         }
     }
 }
